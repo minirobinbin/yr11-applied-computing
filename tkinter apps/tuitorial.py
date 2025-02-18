2fe78c14f9e76690a2f8aaed4c00ed161cb7c592
itspikachugaming <robinxiongxh@gmail.com>
2025-02-19 09:40:44 +1100

    tkinter added

---
+++ tkinter apps/tuitorial.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+root  = tk.Tk()
+window1 = tk.Tk()
+
+window1.title("window 1")
+window1.geometry("300x300+50+50")
+
+root.title("Tk Example")
+root.configure(background="yellow")
+root.minsize(200,200)
+root.maxsize(500,500)
+root.geometry("300x300+50+50")
+
+tk.Label(root, text="Northing will work unless you do.").pack()
+tk.Label(root,text="- Someone").pack()
+
+image = tk.PhotoImage(file="Image copy.gif")
+tk.Label(window1, image=image).pack()
+
+root.mainloop()
+window1.mainloop()
