3019fd94ccb2fae15e4af89c3cc4735e1e8b3684
itspikachugaming <robinxiongxh@gmail.com>
2025-03-11 20:55:08 +1100

    added Calculator

--- tkinter apps/learningfromwebsite/tuitorial1.py
+++ tkinter apps/learningfromwebsite/tuitorial1.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 
 root  = tk.Tk()
-window1 = tk.Tk()
+# window1 = tk.Tk()
 
-window1.title("window 1")
-window1.geometry("300x300+50+50")
+# window1.title("window 1")
+# window1.geometry("300x300+50+50")
 
 root.title("Tk Example")
 root.configure(background="yellow")
@@ -15,8 +15,8 @@ root.geometry("300x300+50+50")
 tk.Label(root, text="Northing will work unless you do.").pack()
 tk.Label(root,text="- Someone").pack()
 
-image = tk.PhotoImage(file="Image copy.gif")
-tk.Label(window1, image=image).pack()
+# image = tk.PhotoImage(file="Image copy.gif")
+# tk.Label(window1, image=image).pack()
 
 root.mainloop()
-window1.mainloop()
+# window1.mainloop()
